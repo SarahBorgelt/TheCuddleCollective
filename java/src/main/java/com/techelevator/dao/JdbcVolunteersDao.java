d1b1d875f2d53061ebbbb7d14c014ceb4c96bb87
Chris Bussey <christina.bussey@icloud.com>
2025-12-08 12:23:49 -0500

    Volunteer work

--- java/src/main/java/com/techelevator/dao/JdbcVolunteersDao.java
+++ java/src/main/java/com/techelevator/dao/JdbcVolunteersDao.java
@@ -3,6 +3,7 @@ package com.techelevator.dao;
 import com.techelevator.model.ShelterVolunteer;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.support.rowset.SqlRowSet;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.ArrayList;
@@ -17,6 +18,8 @@ public class JdbcVolunteersDao implements VolunteerDao{
     }
 
     @Override
+    @PreAuthorize
+    @RequestMapping(path = "/volunteers")
     public List<ShelterVolunteer> getAllVolunteers(){
         List<ShelterVolunteer> shelterVolunteers = new ArrayList<>();
         String sql = "SELECT first_name, last_name, email FROM volunteers";
