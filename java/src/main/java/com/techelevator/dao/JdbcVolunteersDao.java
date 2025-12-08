6f4e64a50a713107fa7474379fee3ad09d864054
Chris Bussey <christina.bussey@icloud.com>
2025-12-08 13:45:47 -0500

    Working on volunteer

--- java/src/main/java/com/techelevator/dao/JdbcVolunteersDao.java
+++ java/src/main/java/com/techelevator/dao/JdbcVolunteersDao.java
@@ -18,8 +18,7 @@ public class JdbcVolunteersDao implements VolunteerDao{
     }
 
     @Override
-    @PreAuthorize
-    @RequestMapping(path = "/volunteers")
+//    @PreAuthorize() //TODO, only volunteers should be able to see all of this information
     public List<ShelterVolunteer> getAllVolunteers(){
         List<ShelterVolunteer> shelterVolunteers = new ArrayList<>();
         String sql = "SELECT first_name, last_name, email FROM volunteers";
